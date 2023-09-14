fbe30befdca67428c42dcebc2154ed767b648088
Raghav Sidharth <raghav.sidharth@live.com>
2023-09-15 00:33:29 +1200

    tailwind not working correctly

---
+++ app/LandingPage.tsx
@@ -0,0 +1,11 @@
+import seaImg from '../public/assets/01.jpg'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <div className="bg-black h-100 w-full bg-cover bg-center">
+        <div></div>
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Image from 'next/image'
 import Navbar from './Navbar'
+import LandingPage from './LandingPage'
 
 export default function Home() {
-  return <Navbar />
+  return (
+    <>
+      <Navbar />
+      <LandingPage />
+
+    </>
+  )
 }
