2b8886697b7b52a83f0b7c633bd1aefc92368a96
Raghav Sidharth <raghav.sidharth@live.com>
2023-09-15 18:17:51 +1200

    Setting up tailwind

--- app/LandingPage.tsx
+++ app/LandingPage.tsx
@@ -3,8 +3,10 @@ import seaImg from '../public/assets/01.jpg'
 export default function LandingPage() {
   return (
     <div>
-      <div className="bg-black h-100 w-full bg-cover bg-center">
-        <div></div>
+      <div>
+        <div>
+          <p>This is the content that will populate the landing page</p>
+        </div>
       </div>
     </div>
   )
